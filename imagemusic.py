1596ff53aba8b60efda4a3b890d3ee8b47fc6f89
Auric Vente <mprops@protonmail.com>
2026-03-05 05:32:59 -0600

    Mods

--- imagemusic.py
+++ imagemusic.py
@@ -7,6 +7,6 @@ def main() -> None:
   audio = sys.argv[2]
   output = sys.argv[3]
 
-  os.popen(f"ffmpeg -loop 1 -i {image} -i {audio} -c:v libx264 -tune stillimage -c:a aac -b:a 320k -pix_fmt yuv420p -shortest {output}.mp4")
+  os.popen(f"ffmpeg -loop 1 -i {image} -i {audio} -c:v libx264 -tune stillimage -vf 'pad=ceil(iw/2)*2:ceil(ih/2)*2' -c:a aac -b:a 320k -pix_fmt yuv420p -shortest {output}.mp4")
 
 if __name__ == "__main__": main()
